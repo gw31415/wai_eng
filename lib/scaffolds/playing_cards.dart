2b6d5d78b9e1d2d86dcbbfaa803e15d6a9151c14
gw31415 <git@amas.dev>
2022-03-21 19:11:34 +0900

    add undo button

--- lib/scaffolds/playing_cards.dart
+++ lib/scaffolds/playing_cards.dart
@@ -37,44 +37,57 @@ class _PlayingCardsScaffoldState extends State<PlayingCardsScaffold> {
           title: Text(widget.flashcards.title),
         ),
         body: SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 8),
-          child: SwipableStack(
-            itemCount: widget.flashcards.length(),
-            swipeAnchor: SwipeAnchor.top,
-            swipeAssistDuration: const Duration(milliseconds: 100),
-            detectableSwipeDirections: const {
-              SwipeDirection.right,
-              SwipeDirection.left,
-            },
-            swipeNextOnSwipeCanceled: const SwipeNextArgs(
-              swipeDirection: SwipeDirection.down,
-              shouldCallCompletionCallback: true,
-              ignoreOnWillMoveNext: false,
-            ),
-            overlayBuilder: (_, properties) {
-              final card = widget.flashcards.get(properties.index);
-              if (card == null) {
-                return Container();
-              }
-              return Card(child: Center(child: card.answer));
-            },
-            stackClipBehaviour: Clip.none,
-            builder: (context, properties) {
-              final card = widget.flashcards.get(properties.index);
-              if (card == null) {
-                return Container();
-              }
-              return Card(child: Center(child: card.question));
-            },
-            onSwipeCompleted: (i, direction) {
-              switch (direction) {
-                case SwipeDirection.down:
-                  break;
-                default:
-              }
-            },
-          ),
-        )));
+            child: Stack(children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 8),
+              child: SwipableStack(
+                controller: _controller,
+                itemCount: widget.flashcards.length(),
+                swipeAnchor: SwipeAnchor.top,
+                swipeAssistDuration: const Duration(milliseconds: 100),
+                detectableSwipeDirections: const {
+                  SwipeDirection.right,
+                  SwipeDirection.left,
+                },
+                swipeNextOnSwipeCanceled: const SwipeNextArgs(
+                  swipeDirection: SwipeDirection.down,
+                  shouldCallCompletionCallback: true,
+                  ignoreOnWillMoveNext: false,
+                ),
+                overlayBuilder: (_, properties) {
+                  final card = widget.flashcards.get(properties.index);
+                  if (card == null) {
+                    return Container();
+                  }
+                  return Card(child: Center(child: card.answer));
+                },
+                stackClipBehaviour: Clip.none,
+                builder: (context, properties) {
+                  final card = widget.flashcards.get(properties.index);
+                  if (card == null) {
+                    return Container();
+                  }
+                  return Card(child: Center(child: card.question));
+                },
+                onSwipeCompleted: (i, direction) {
+                  switch (direction) {
+                    case SwipeDirection.down:
+                      break;
+                    default:
+                  }
+                },
+              )),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 100,
+                child: Center(
+                    child: IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: _controller.canRewind ? _controller.rewind : null,
+				  tooltip: "Undo",
+                )),
+              ))
+        ])));
   }
 }
