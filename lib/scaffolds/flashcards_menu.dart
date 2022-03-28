e5d495b2ee57142fc9756081b5751b8e0e9478ae
gw31415 <git@amas.dev>
2022-03-28 21:00:02 +0900

    add dialog to manage multiple commands to flashcards

--- lib/scaffolds/flashcards_menu.dart
+++ lib/scaffolds/flashcards_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../modules/flashcard.dart';
 import './book_player.dart';
+import './book_table_viewer.dart';
 
 class _FlashCardsListView extends StatelessWidget {
   final List<FlashCardBook> flashcards;
@@ -11,16 +12,59 @@ class _FlashCardsListView extends StatelessWidget {
     return ListView.builder(
         itemCount: flashcards.length,
         itemBuilder: (context, index) {
+          // ダイアログの構築
+          List<Widget> dialogItems = [];
           final cards = flashcards[index];
-          return ListTile(
-            title: Text(cards.title),
-            onTap: () {
+          _openBookPlayer() {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return FlashCardBookPlayerScaffold(
+                book: cards,
+              );
+            }));
+          }
+
+          dialogItems.add(
+            SimpleDialogOption(
+              onPressed: () => Navigator.pop(context, _openBookPlayer),
+              child: const Text('開く'),
+            ),
+          );
+          if (cards is FlashCardBookWithBody) {
+            _openBookTable() {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return FlashCardBookPlayerScaffold(
+                return BookTableScaffold(
                   book: cards,
                 );
               }));
-            },
+            }
+
+            dialogItems.add(
+              SimpleDialogOption(
+                onPressed: () => Navigator.pop(context, _openBookTable),
+                child: const Text('内容の閲覧'),
+              ),
+            );
+          }
+
+          return ListTile(
+            title: Text(cards.title),
+            onTap: _openBookPlayer,
+            trailing: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                Future.microtask(() async {
+                  final nextTask = await showDialog<Function>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SimpleDialog(
+                          title: Text(cards.title),
+                          children: dialogItems,
+                        );
+                      });
+                  nextTask!();
+                });
+              },
+            ),
           );
         });
   }
