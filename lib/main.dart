f705f28b20e017b0e9afdc18fe1db565baa13a3f
gw31415 <git@amas.dev>
2022-10-07 15:48:19 +0900

    read license by loadString from file: lib/modules/swipable_stack/LICENSE

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'scaffolds/flashcards_menu.dart';
 import 'modules/flashcard.dart';
 import 'modules/flashcardbook.dart';
@@ -8,31 +9,11 @@ import 'modules/httpgetcache.dart';
 
 void main() {
   LicenseRegistry.addLicense(() {
-    return Stream<LicenseEntry>.fromIterable(<LicenseEntry>[
-      const LicenseEntryWithLineBreaks(<String>['swipable_stack'], '''
-        MIT License
-
-        Copyright (c) 2021 Ryunosuke Watanabe
-
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-		'''),
-    ]);
+    return Stream.fromFuture((() async {
+      final licence =
+          await rootBundle.loadString('lib/modules/swipable_stack/LICENSE');
+      return LicenseEntryWithLineBreaks(['swipable_stack'], licence);
+    })());
   });
   runApp(const MainApp());
 }
