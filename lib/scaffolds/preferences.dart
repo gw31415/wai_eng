54b272192579a092b140b06097094f0ff3d9a2da
gw31415 <git@amas.dev>
2022-10-11 16:59:12 +0900

    format & fix dispose bug

--- lib/scaffolds/preferences.dart
+++ lib/scaffolds/preferences.dart
@@ -24,7 +24,6 @@ class _PreferencesScaffoldState extends State<PreferencesScaffold> {
     final _futureSubscription = [
       PreferencesManager.wakelock.listener((data) {
         if (!mounted) {
-		  dispose();
           return;
         }
         setState(() {
@@ -34,41 +33,44 @@ class _PreferencesScaffoldState extends State<PreferencesScaffold> {
     ];
     return Scaffold(
       appBar: AppBar(title: const Text("設定")),
-      body: FutureBuilder(future: (() async {
-        List<ListenerSubscription> res = [];
-        for (var element in _futureSubscription) {
-          res.add(await element);
-        }
-        return res;
-      })(), builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("${snapshot.error}"),
+      body: FutureBuilder(
+        future: (() async {
+          List<ListenerSubscription> res = [];
+          for (var element in _futureSubscription) {
+            res.add(await element);
+          }
+          return res;
+        })(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            }
+            return Container(
+              decoration: BoxDecoration(color: Theme.of(context).splashColor),
+              child: const Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
             );
           }
-          return Container(
-            decoration: BoxDecoration(color: Theme.of(context).splashColor),
-            child: const Center(
-              child: CircularProgressIndicator.adaptive(),
-            ),
+          _subscriptions = snapshot.data as List<ListenerSubscription>;
+          return SettingsList(
+            sections: [
+              SettingsSection(
+                title: const Text('フラッシュカード プレイ画面'),
+                tiles: [
+                  SettingsTile.switchTile(
+                      initialValue: _wakelock,
+                      onToggle: PreferencesManager.wakelock.setter,
+                      title: const Text('画面消灯を抑制する'))
+                ],
+              ),
+            ],
           );
-        }
-        _subscriptions = snapshot.data as List<ListenerSubscription>;
-        return SettingsList(
-          sections: [
-            SettingsSection(
-              title: const Text('フラッシュカード プレイ画面'),
-              tiles: [
-                SettingsTile.switchTile(
-                    initialValue: _wakelock,
-                    onToggle: PreferencesManager.wakelock.setter,
-                    title: const Text('画面消灯を抑制する'))
-              ],
-            ),
-          ],
-        );
-      }),
+        },
+      ),
     );
   }
 }
